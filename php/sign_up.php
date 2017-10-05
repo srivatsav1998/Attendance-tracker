d862d9ca482c615ca2c66ad7edde456049a8fc6d
Srivatsav Gunisetty <srivatsav1998@gmail.com>
2017-10-05 17:54:43 +0530

    Restricted the repetition of usernames.

--- php/sign_up.php
+++ php/sign_up.php
@@ -3,21 +3,26 @@
 	$conn = Connect();
 	if(isset($_POST['signup'])){
 		$full_name = $_POST["first_name"] ." ". $_POST["last_name"];
-		echo $full_name;
 		$reg_no = $_POST["reg_no"];
 		$year = $_POST["year_of_admission"];
 		$branch = $_POST["branch"];
-		$username = stripslashes($_POST["username"]); 
+		$username = stripslashes($_POST["username"]);
 		$username = mysqli_real_escape_string($conn,$_POST["username"]);
-		$password = stripslashes($_POST["password"]); 
-		$password = mysqli_real_escape_string($conn,$_POST["password"]); 
+		$password = stripslashes($_POST["password"]);
+		$password = mysqli_real_escape_string($conn,$_POST["password"]);
 		$pass = md5($password);
-		$myquery = "INSERT into permanent_list (user_name, password, name, reg_no, year_of_admission, branch) VALUES ('$username','$pass','$full_name','$reg_no','$year', '$branch')";
-		$success = $conn->query($myquery);
-		header("location: /login.html");
- 
-		if (!$success) {
-		    die("Couldn't enter data: ".$conn->error); 
+  	$result = mysqli_query($conn,"SELECT * FROM permanent_list WHERE user_name='" . $_POST["username"] . "'");
+	  $user_count = mysqli_num_rows($result);
+		if($user_count>0) {
+	      echo "<script type='text/javascript'>alert('Username is already in use'); window.location.href = '/login.html';</script>";
+	  }else{
+			$myquery = "INSERT into permanent_list (user_name, password, name, reg_no, year_of_admission, branch) VALUES ('$username','$pass','$full_name','$reg_no','$year', '$branch')";
+			$success = $conn->query($myquery);
+			header("location: /login.html");
+
+			if (!$success) {
+			    die("Couldn't enter data: ".$conn->error);
+			}
 		}
 	}
 	$conn->close();
