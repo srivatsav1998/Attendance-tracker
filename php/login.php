d14fc56ae02f8c9d0db458ad3963595fdf2ca614
Srivatsav Gunisetty <srivatsav1998@gmail.com>
2017-10-10 11:31:24 +0530

    Restricted the user to be in the location and give appropriate permissions.

--- php/login.php
+++ php/login.php
@@ -2,26 +2,26 @@
 	require 'connection.php';
 	session_start();
 	$conn = Connect();
-	if(isset($_POST['login'])){
-		$username = stripslashes($_POST["username"]); 
+	if(isset($_POST['login']) and $_COOKIE['location']=="yes"){
+		$username = stripslashes($_POST["username"]);
 		$username = mysqli_real_escape_string($conn,$_POST["username"]);
-		$password = stripslashes($_POST["password"]); 
-		$password = mysqli_real_escape_string($conn,$_POST["password"]); 
+		$password = stripslashes($_POST["password"]);
+		$password = mysqli_real_escape_string($conn,$_POST["password"]);
 		$pass = md5($password);
 		$unique_id = $_POST["unique_id"];
 		$myquery = "SELECT user_name, password FROM permanent_list WHERE user_name = '$username' AND password = '$pass' AND unique_id = '$unique_id' AND approval = 0";
 		$result = $conn->query($myquery);
-        if(mysqli_num_rows($result) > 0 ) { 
-            $_SESSION["logged_in"] = true; 
+        if(mysqli_num_rows($result) > 0 ) {
+            $_SESSION["logged_in"] = true;
             $_SESSION["username"] = $username;
             $get_data = mysqli_query($conn,"SELECT * FROM permanent_list WHERE user_name = '$username'");
             $roll_no = mysqli_fetch_assoc($get_data);
             $roll_no = $roll_no["reg_no"];
             setcookie('login', $username ,time()+7200,"/");
-            setcookie('password', $pass, time()+7200,"/");	
+            setcookie('password', $pass, time()+7200,"/");
             $time = date("Y-m-d H:i:s");
             $time_obj = new DateTime($time);
-            $_SESSION["login_time"] = $time_obj; 
+            $_SESSION["login_time"] = $time_obj;
 			$date = date("d-m-Y");
 			if($username != "admin"){
 				$query = "INSERT into attendance (user_name, reg_no, login_date, login_time) VALUES ('$username','$roll_no','$date','$time')";
@@ -31,7 +31,7 @@
 				}
 				else{
 					header("location: /student/student.html");
-				}			
+				}
 			}
 			else{
 				header("location: /admin/admin.html");
@@ -40,6 +40,9 @@
         else{
         	echo "incorrect credentials! :(";
         }
+	}
+	else{
+		echo "<script type='text/javascript'>alert('You have either not given the permission to know your location or you are not in the proximity of lab! Please give the appropriate permissions and try again'); window.location.href = '/login.html';</script>";
 	}
 	$conn->close();
 ?>
