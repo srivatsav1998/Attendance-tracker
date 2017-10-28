4acc38f4d8137e957fbc5cd1bd2ec3588b91e36c
Ömer Faruk ÇAKI <OFarukCaki@users.noreply.github.com>
2017-10-29 01:53:47 +0300

    Update admin_logout.php

--- admin/backend/admin_logout.php
+++ admin/backend/admin_logout.php
@@ -1,10 +1,19 @@
 <?php
+	// Import connection settings
 	require 'connection.php';
+
+	//start the session and establish a connection
 	session_start();
 	$conn = Connect();
+
+	// Destroy the current session and deleting cookies.
 	session_destroy();
 	setcookie('login',"",time()-3600,'/');
 	setcookie('password',"",time()-3600,'/');
+
+	// Forwarding to login page
 	header("location: /login.html");
+
+	// lastly close the connection when done.
 	$conn->close();
 ?>
